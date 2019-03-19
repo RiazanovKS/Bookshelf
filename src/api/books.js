0b5b2c143369657de8616318f0628f22c7de109e
Kirill Riazanov <riazanovkiril99@gmail.com>
2019-03-20 00:59:30 +0300

    Promise has been replased by axios

--- src/api/books.js
+++ src/api/books.js
@@ -1,2 +1,10 @@
+import axios from 'axios';
+
+const API_KEY = 'FOn4xsGYPgcO962L5Siyg';
+const URL = 'https://www.goodreads.com';
+const SEARCH_PATH = '/search.xml'
+const QUERY = '';
+
 // вместо Promise вставь метод из axios
-export const fetchBooks = () => Promise.resolve({ data: "Books list" });
+// export const fetchBooks = () => Axios.get(`${URL}${SEARCH_PATH}?key=${API_KEY}?q=${QUERY}`);
+export const fetchBooks = () => axios.get(`${URL}${SEARCH_PATH}?key=${API_KEY}?q=${QUERY}`);
